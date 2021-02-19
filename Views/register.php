33780f6b6c5216333b6738ac88dc27c1f612f973
ReactDev313 <reactist313@hotmail.com>
2021-02-18 18:39:47 -0600

    update user controller

--- Views/register.php
+++ Views/register.php
@@ -18,7 +18,7 @@
             <!-- <div class="form-group">
                 <input type="password" class="form-control" placeholder="Confirm Password" required="">
             </div> -->
-
+            <input type="hidden" name="register[role]" id="editId" value="1">
             <button type="submit" class="form-button button-l margin-b">Sign Up</button>
 
             <a class="text-darkyellow" href="/login"><small>Sign In</small></a>
